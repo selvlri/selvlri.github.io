1c0e30ed9ee23ee51a64202d1e83e4bc79295f32
selvlri <vleriasel34@mail.ru>
2021-05-22 06:41:09 +0000

    first commit

---
+++ knapsack.js
@@ -0,0 +1,98 @@
+const items = buildItems()
+let res = genSolution(weightFilter(20, genPowerSet(items)))
+console.log(res.totalValue)
+console.log(res.taken)
+
+function value(taken)
+{
+	let totalValue = 0
+	for(let item of taken) totalValue += item.value
+	return totalValue
+}
+function genSolution(xFGen)
+{
+	let totalValue = 0
+	let taken = []
+	for (let x of xFGen)
+	{
+		if (value(x) > totalValue)
+		{
+			totalValue = value(x)
+			taken = x.slice()
+		}
+	}
+	return {"totalValue": totalValue, "taken": taken}
+}
+function* weightFilter(maxWeight, genPowerSet)
+{
+	for(let taken of genPowerSet)
+	{
+		if (weightTest(taken, maxWeight))
+			yield taken
+	}
+}
+function weightTest(taken, maxWeight)
+{
+	totalWeight = 0
+	for(let t of taken)
+	{
+		totalWeight += t.weight
+		if(totalWeight > maxWeight) return false
+	}
+	return true
+}
+
+function* genPowerSet(items)
+{
+	for(let x of genBin(items.length))
+	{
+		let taken = []
+		for (let i = 0; i < x.length; i++)
+		{
+			if (x[i] == 1) taken.push(items[i])
+		}
+		yield taken
+	}
+}
+
+function* genBin(n)
+{
+	let b = []
+	for(let i = 0; i < n; i++) b.push(0)
+	yield b
+	let i = 0
+	while(true)
+	{
+		if(i == n) return
+		else if( b[i] == 1)
+		{
+			b[i] = 0
+			i++
+		}
+		else
+		{
+			b[i] = 1
+			i = 0
+			yield b
+		}
+	}
+}
+
+
+function buildItems()
+{
+	const names = ['часы', 'картина', 'радио', 'ваза', 'книга', 'компьютер']
+	const values = [175, 90, 20, 50, 10, 200]
+	const weights = [10, 9, 4, 2, 1, 20]
+	const items = []
+	for (let i = 0; i < values.length; i++)
+	{
+		items.push(
+		    {
+			name:names[i],
+			value: values[i],
+			weight: weights[i]
+		})
+	}
+	return items
+}
